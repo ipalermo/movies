a1fb3be26be68cc6fe362e8542f3e7e98f47b313
Ignacio Palermo <nacho.palermo84@gmail.com>
2022-11-17 18:29:04 +0000

    Build gradle file for convention module.

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.ipalermo.movies.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplicationCompose") {
+            id = "movies.android.application.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        }
+        register("androidApplication") {
+            id = "movies.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+        register("androidApplicationJacoco") {
+            id = "movies.android.application.jacoco"
+            implementationClass = "AndroidApplicationJacocoConventionPlugin"
+        }
+        register("androidLibraryCompose") {
+            id = "movies.android.library.compose"
+            implementationClass = "AndroidLibraryComposeConventionPlugin"
+        }
+        register("androidLibrary") {
+            id = "movies.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+        register("androidFeature") {
+            id = "movies.android.feature"
+            implementationClass = "AndroidFeatureConventionPlugin"
+        }
+        register("androidLibraryJacoco") {
+            id = "movies.android.library.jacoco"
+            implementationClass = "AndroidLibraryJacocoConventionPlugin"
+        }
+        register("androidTest") {
+            id = "movies.android.test"
+            implementationClass = "AndroidTestConventionPlugin"
+        }
+        register("androidHilt") {
+            id = "movies.android.hilt"
+            implementationClass = "AndroidHiltConventionPlugin"
+        }
+        register("firebase-perf") {
+            id = "movies.firebase-perf"
+            implementationClass = "FirebasePerfConventionPlugin"
+        }
+    }
+}
