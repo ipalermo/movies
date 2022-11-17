0db67dfe16ffa228348ec931bf1ec6d82143a6a1
Ignacio Palermo <nacho.palermo84@gmail.com>
2022-11-17 17:33:54 +0000

    Add application class.

---
+++ app/src/main/java/com/ipalermo/movies/MoviesApplication.kt
@@ -0,0 +1,12 @@
+package com.ipalermo.movies
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * [Application] class for Movies
+ */
+@HiltAndroidApp
+class MoviesApplication : Application() {
+
+}
