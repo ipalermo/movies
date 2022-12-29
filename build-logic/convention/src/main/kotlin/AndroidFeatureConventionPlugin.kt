038e49f8abe38b235d3be4a85b846f862e070a27
Ignacio Palermo <nacho.palermo84@gmail.com>
2022-12-29 10:22:12 +0000

    Add domain module dependency.

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -29,7 +29,6 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                 add("implementation", project(":core:designsystem"))
                 add("implementation", project(":core:data"))
                 add("implementation", project(":core:common"))
-                add("implementation", project(":core:domain"))
 
                 add("testImplementation", kotlin("test"))
                 add("testImplementation", project(":core:testing"))
