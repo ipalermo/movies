ff60478e1f129c34803aad1096d3885989d0f5b8
Ignacio Palermo <nacho.palermo84@gmail.com>
2022-12-29 10:49:43 +0000

    Set content. Enable and disable JankStats. Inject network monitor.

--- app/src/main/java/com/ipalermo/movies/MainActivity.kt
+++ app/src/main/java/com/ipalermo/movies/MainActivity.kt
@@ -3,14 +3,65 @@ package com.ipalermo.movies
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.DisposableEffect
+import androidx.core.view.WindowCompat
+import androidx.metrics.performance.JankStats
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.ipalermo.movies.core.data.util.NetworkMonitor
+import com.ipalermo.movies.core.designsystem.theme.MoviesTheme
+import com.ipalermo.movies.ui.MoviesApp
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    /**
+     * Lazily inject [JankStats], which is used to track jank throughout the app.
+     */
+    @Inject
+    lateinit var lazyStats: dagger.Lazy<JankStats>
+
+    @Inject
+    lateinit var networkMonitor: NetworkMonitor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
 
+        // Turn off the decor fitting system windows, which allows us to handle insets,
+        // including IME animations
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContent {
+            val systemUiController = rememberSystemUiController()
+            val darkTheme = isSystemInDarkTheme()
+
+            // Update the dark content of the system bars to match the theme
+            DisposableEffect(systemUiController, darkTheme) {
+                systemUiController.systemBarsDarkContentEnabled = !darkTheme
+                onDispose {}
+            }
+
+            MoviesTheme {
+                MoviesApp(
+                    networkMonitor = networkMonitor,
+                    windowSizeClass = calculateWindowSizeClass(this),
+                )
+            }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lazyStats.get().isTrackingEnabled = true
+    }
+
+    override fun onPause() {
+        super.onPause()
+        lazyStats.get().isTrackingEnabled = false
     }
 }
