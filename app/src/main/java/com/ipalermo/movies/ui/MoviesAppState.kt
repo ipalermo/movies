60eee21a2195b111d786fc715877651fdf6ea640
Ignacio Palermo <nacho.palermo84@gmail.com>
2022-12-29 10:51:59 +0000

    Add rememberMoviesAppState composable.

---
+++ app/src/main/java/com/ipalermo/movies/ui/MoviesAppState.kt
@@ -0,0 +1,114 @@
+package com.ipalermo.movies.ui
+
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
+import androidx.tracing.trace
+import com.ipalermo.movies.core.data.util.NetworkMonitor
+import com.ipalermo.movies.core.ui.TrackDisposableJank
+import com.ipalermo.movies.feature.discover.navigation.discoverRoute
+import com.ipalermo.movies.feature.discover.navigation.navigateToDiscoverGraph
+import com.ipalermo.movies.navigation.TopLevelDestination
+import com.ipalermo.movies.navigation.TopLevelDestination.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+@Composable
+fun rememberMoviesAppState(
+    windowSizeClass: WindowSizeClass,
+    networkMonitor: NetworkMonitor,
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    navController: NavHostController = rememberNavController()
+): MoviesAppState {
+    NavigationTrackingSideEffect(navController)
+    return remember(navController, coroutineScope, windowSizeClass, networkMonitor) {
+        MoviesAppState(navController, coroutineScope, networkMonitor)
+    }
+}
+
+@Stable
+class MoviesAppState(
+    val navController: NavHostController,
+    val coroutineScope: CoroutineScope,
+    networkMonitor: NetworkMonitor,
+) {
+    private val currentDestination: NavDestination?
+        @Composable get() = navController
+            .currentBackStackEntryAsState().value?.destination
+
+    val currentTopLevelDestination: TopLevelDestination?
+        @Composable get() = when (currentDestination?.route) {
+            discoverRoute -> DISCOVER
+            else -> null
+        }
+
+    val isOffline = networkMonitor.isOnline
+        .map(Boolean::not)
+        .stateIn(
+            scope = coroutineScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = false
+        )
+
+    /**
+     * UI logic for navigating to a top level destination in the app. Top level destinations have
+     * only one copy of the destination of the back stack, and save and restore state whenever you
+     * navigate to and from it.
+     *
+     * @param topLevelDestination: The destination the app needs to navigate to.
+     */
+    fun navigateToTopLevelDestination(topLevelDestination: TopLevelDestination) {
+        trace("Navigation: ${topLevelDestination.name}") {
+            val topLevelNavOptions = navOptions {
+                // Pop up to the start destination of the graph to
+                // avoid building up a large stack of destinations
+                // on the back stack as users select items
+                popUpTo(navController.graph.findStartDestination().id) {
+                    saveState = true
+                }
+                // Avoid multiple copies of the same destination when
+                // reselecting the same item
+                launchSingleTop = true
+                // Restore state when reselecting a previously selected item
+                restoreState = true
+            }
+
+            when (topLevelDestination) {
+                DISCOVER -> navController.navigateToDiscoverGraph(topLevelNavOptions)
+            }
+        }
+    }
+
+    fun onBackClick() {
+        navController.popBackStack()
+    }
+}
+
+/**
+ * Stores information about navigation events to be used with JankStats
+ */
+@Composable
+private fun NavigationTrackingSideEffect(navController: NavHostController) {
+    TrackDisposableJank(navController) { metricsHolder ->
+        val listener = NavController.OnDestinationChangedListener { _, destination, _ ->
+            metricsHolder.state?.putState("Navigation", destination.route.toString())
+        }
+
+        navController.addOnDestinationChangedListener(listener)
+
+        onDispose {
+            navController.removeOnDestinationChangedListener(listener)
+        }
+    }
+}
